dc083db3f179b6dff917e34741731cabf60de8da
lanchii <sekerija.lana@gmail.com>
2023-11-23 15:45:57 +0100

    fix path

--- app/backend/src/main/java/at/htl/feature/event/EventResource.java
+++ app/backend/src/main/java/at/htl/feature/event/EventResource.java
@@ -18,6 +18,7 @@ public class EventResource {
     }
 
     @POST
+    @Transactional
     @Path("/removeEvent/{id}")
     public void removeEvent (@PathParam("id") int id){
         eventDao.removeEvents(id);
@@ -25,6 +26,7 @@ public class EventResource {
 
     @POST
     @Path("/addEvent")
+    @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     public void addEvent(Event event){
         eventDao.addEvent(event);
