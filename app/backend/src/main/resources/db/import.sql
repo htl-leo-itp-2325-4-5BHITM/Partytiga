5bc48ad95b96f2199e845b9f40fed328be5dd51f
heikov3006 <h.vichtbauer@students.htl-leonding.ac.at>
2023-12-01 09:42:40 +0100

    little fixes

--- app/backend/src/main/resources/db/import.sql
+++ app/backend/src/main/resources/db/import.sql
@@ -1,2 +1,2 @@
---INSERT INTO public.tb_event ("date", "location", "name", organization) VALUES('2023-11-16', 'HTL-Leonding', 'Birthday', 'Krenn');
---INSERT INTO public.tb_event ("date", "location", "name", organization) VALUES('2024-03-11', 'Kürnberghalle', 'HTL-Ball', 'Kümmerer');
+INSERT INTO public.tb_event ("date", "location", "name", organization) VALUES('2023-11-16', 'HTL-Leonding', 'Birthday', 'Krenn');
+INSERT INTO public.tb_event ("date", "location", "name", organization) VALUES('2024-03-11', 'Kürnberghalle', 'HTL-Ball', 'Kümmerer');
