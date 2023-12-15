8ab2f6391f55a41474b3342588cce8a729b8d680
heikov3006 <h.vichtbauer@students.htl-leonding.ac.at>
2023-12-15 08:50:41 +0100

    fixed style and prettier  code

--- app/frontend/src/components/event-table-component.ts
+++ app/frontend/src/components/event-table-component.ts
@@ -1,54 +1,69 @@
-import {Event, store} from "../model/model"
-import {html, render} from "lit-html"
-import { updateEvent, removeEvent } from "../index"
+import { Event, store } from "../model/model";
+import { html, render } from "lit-html";
+import { updateEvent, removeEvent, alterEvent } from "../index";
 //addEvent
 
 const rowTemplate = (event: Event) => html`
-<div>
-<tr>
+  <tr class="tableRow" @click=${() => alterEvent(event)}>
     <td>${event.name}</td>
     <td>${event.organization}</td>
     <td>${event.date}</td>
     <td>${event.location}</td>
-</tr>
-    <button @click=${() => removeEvent(event.id)}>remove</button>
-    <button id="openUpdateModalButton" @click=${() => updateEvent(event.id,event)}>edit</button>
-</div>
-`
+  </tr>
+`;
+
+/**
+ * <button @click=${() => removeEvent(event.id)}>remove</button>
+  <button
+    id="openUpdateModalButton"
+    @click=${() => updateEvent(event.id, event)}>edit</button> 
+ */
+
 const template = (events: Event[]) => {
-    const rows = events.map(rowTemplate)
-    return html`
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<table class="w3-table w3-striped w3-bordered">
-    <caption>Events</caption>
-    <thead>
+  const rows = events.map(rowTemplate);
+  return html`
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+
+    <style>
+    
+      .tableRow{
+        cursor:pointer;
+      }
+    
+    </style>
+
+    <table class="w3-table w3-striped w3-bordered w3-hoverable">
+      <caption>
+        <h2>Events</h2>
+      </caption>
+      <thead class="w3-light-grey">
         <tr>
-        <td>Name</td>
-        <td>Organizer</td>
-        <td>Date</td>
-        <td>Location</td>
+          <td><h4>Name</h4></td>
+          <td><h4>Organizer</h4></td>
+          <td><h4>Date</h4></td>
+          <td><h4>Location</h4></td>
         </tr>
-    </thead>
-    <tbody>
+      </thead>
+      <tbody>
         ${rows}
-    </tbody>
-</table>
-`
-}
+      </tbody>
+    </table>
+  `;
+};
 class EventTableComponent extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({mode: "open"})
-    }
-    connectedCallback() {
-        console.log("EventTable loaded")
-        store.subscribe(model => {
-            this.render(model.events)
-        })
-    }
-    render(events: Event[]) {
-        console.log("render", events)
-        render(template(events), this.shadowRoot)
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  connectedCallback() {
+    console.log("EventTable loaded");
+    store.subscribe((model) => {
+      this.render(model.events);
+    });
+  }
+  render(events: Event[]) {
+    console.log("render", events);
+    render(template(events), this.shadowRoot);
+  }
 }
-customElements.define("event-table", EventTableComponent)
+customElements.define("event-table", EventTableComponent);
--- app/frontend/src/index.ts
+++ app/frontend/src/index.ts
@@ -6,65 +6,42 @@ import { animationFrameScheduler } from "rxjs";
 const addEventButton = document.getElementById("addEvent") as HTMLButtonElement;
 addEventButton.addEventListener("click", addEvent);
 
+
+const removeEventButton = document.getElementById("removeEvent") as HTMLButtonElement;
+addEventButton.addEventListener("click", () => removeEvent);
+
 const modal = document.getElementById("myModal");
-const modalUpdate = document.getElementById("myModalUpdate");
 
 //add modal
 const closeButton = document.getElementById("close");
-closeButton.addEventListener("click", closeModal)
+closeButton.addEventListener("click", closeModal);
 
-const openModalButton = document.getElementById('openModalButton')
-openModalButton.addEventListener('click', openModal)
-
-const closeUpdateButton = document.getElementById("closeUpdate");
-closeUpdateButton.addEventListener("click", closeModalUpdate)
-
-document.addEventListener('DOMContentLoaded', function () {
-  const openUpdateModalButton = document.getElementById('openUpdateModalButton')
-
-  if (openUpdateModalButton) {
-    openUpdateModalButton.addEventListener('click', openModalUpdate);
-  } else {
-    console.log("Element mit der ID 'openUpdateModalButton' nicht gefunden.");
-  }
-});
-
-const successBox = document.getElementById("success");
-const errorBox = document.getElementById("error");
+const openModalButton = document.getElementById("openModalButton");
+openModalButton.addEventListener("click", openModal);
 
 loadEvents();
 
 function openModal() {
-  modal.classList.add("open")
-  console.log("add modal open")
+  modal.classList.add("open");
+  console.log("add modal open");
 }
 
 // When the user clicks on <span> (x), close the modal
 function closeModal() {
-  //modal.style.display = "none";
-  modal.classList.remove("open")
-}
-
-function openModalUpdate() {
-  modalUpdate.classList.add("open")
-  console.log("update modal open")
-}
-
-function closeModalUpdate() {
-  modalUpdate.classList.remove("open")
+  resetForm();
+  modal.classList.remove("open");
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
-    //modal.style.display = "none";
-    modal.classList.remove("open")
-
+    resetForm();
+    modal.classList.remove("open");
   }
-}
+};
 
 export function addEvent() {
-  console.log("addEvent start")
+  console.log("addEvent start");
 
   let eventName: string = (
     document.getElementById("eventName") as HTMLInputElement
@@ -81,122 +58,115 @@ export function addEvent() {
 
   console.log("yes");
 
-    if (
-      eventName != "" &&
-      organizerName != "" &&
-      eventDate != "" &&
-      eventLocation != ""
-    ) {
-      const event: Event = {
-        id: null,
-        name: eventName,
-        organization: organizerName,
-        date: eventDate,
-        location: eventLocation,
-      };
-      console.log(event);
-  
-      fetch("/api/events/addEvent", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(event),
+  if (
+    eventName != "" &&
+    organizerName != "" &&
+    eventDate != "" &&
+    eventLocation != ""
+  ) {
+    const event: Event = {
+      id: null,
+      name: eventName,
+      organization: organizerName,
+      date: eventDate,
+      location: eventLocation,
+    };
+    console.log(event);
+
+    fetch("/api/events/addEvent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event),
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          console.log("Erfolgreiche Datenübertragung ins backend!");
+          (document.getElementById("eventName") as HTMLInputElement).value = "";
+          (document.getElementById("organizerName") as HTMLInputElement).value =
+            "";
+          (document.getElementById("eventDate") as HTMLInputElement).value = "";
+          (document.getElementById("eventLocation") as HTMLInputElement).value =
+            "";
+          loadEvents();
+        } else {
+          console.error("Fehler beim Speichern der Daten.");
+        }
       })
-        .then((response) => {
-          console.log(response)
-          if (response.ok) {
-            console.log("Erfolgreiche Datenübertragung ins backend!");
-            (
-              document.getElementById("eventName") as HTMLInputElement
-            ).value = "";
-            (
-              document.getElementById("organizerName") as HTMLInputElement
-            ).value = "";
-            (
-              document.getElementById("eventDate") as HTMLInputElement
-            ).value = "";
-            (
-              document.getElementById("eventLocation") as HTMLInputElement
-            ).value = "";
-            loadEvents()
-          } else {
-            console.error("Fehler beim Speichern der Daten.");
-          }
-        })
-        .catch((error) => {
-          console.error("Fehler beim Speichern der Daten:", error);
-        });
-    } else {
-      console.log("no");
-    }
-  closeModal()
+      .catch((error) => {
+        console.error("Fehler beim Speichern der Daten:", error);
+      });
+  } else {
+    console.log("no");
+  }
+  closeModal();
 }
 
-export function updateEvent(eventId: Number, event: Event) {
-  openModalUpdate()
-  console.log(eventId)
-  
-  //anzeigen der alten daten in den eingabefeldern
+export function alterEvent(event: Event) {
+  openModal();
+  changeSaveButton(event);
   displayEventData(event);
+}
 
-  const updateEventButton = document.getElementById("updateEvent") as HTMLButtonElement;
-  updateEventButton.addEventListener("click", () => {
+function changeSaveButton(event: Event) {
+  addEventButton.removeEventListener("click", addEvent);
+  addEventButton.addEventListener("click", () => updateEvent(event));
+}
 
+export function updateEvent(event: Event) {
   let eventName: string = (
-    document.getElementById("eventNameUpdate") as HTMLInputElement
+    document.getElementById("eventName") as HTMLInputElement
   ).value;
   let organizerName: string = (
-    document.getElementById("organizerNameUpdate") as HTMLInputElement
+    document.getElementById("organizerName") as HTMLInputElement
   ).value;
   let eventDate: string = (
-    document.getElementById("eventDateUpdate") as HTMLInputElement
+    document.getElementById("eventDate") as HTMLInputElement
   ).value;
   let eventLocation: string = (
-    document.getElementById("eventLocationUpdate") as HTMLInputElement
+    document.getElementById("eventLocation") as HTMLInputElement
   ).value;
 
-    console.log("clicked update button")
-    if (
-      eventName !== "" &&
-      organizerName !== "" &&
-      eventDate !== "" &&
-      eventLocation !== ""
-    ) {
-      const updatedEvent: Event = {
-        id: eventId,
-        name: eventName,
-        organization: organizerName,
-        date: eventDate,
-        location: eventLocation,
-      };
-      console.log(updatedEvent)
-        fetch("/api/events/updateEvent", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedEvent),
-        })
-          .then((response) => {
-            if (response.ok) {
-              console.log("Erfolgreiche Datenübertragung ins backend!");
-              resetForm();
-              loadEvents()
-            } else {
-              console.error("Fehler beim Updaten der Daten.");
-            }
-          })
-          .catch((error) => {
-            console.error("Fehler beim Updaten der Daten:", error);
-          });
-          closeModalUpdate();
-      } else{
-        console.log("wrong data inserted")
-      }
-  });
+  console.log("clicked update button");
+  if (
+    eventName !== "" &&
+    organizerName !== "" &&
+    eventDate !== "" &&
+    eventLocation !== ""
+  ) {
+    const updatedEvent: Event = {
+      id: event.id,
+      name: eventName,
+      organization: organizerName,
+      date: eventDate,
+      location: eventLocation,
+    };
+    console.log(updatedEvent);
+    fetch("/api/events/updateEvent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedEvent),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Erfolgreiche Datenübertragung ins backend!");
+          loadEvents();
+        } else {
+          console.error("Fehler beim Updaten der Daten.");
+        }
+      })
+      .catch((error) => {
+        console.error("Fehler beim Updaten der Daten:", error);
+      });
+    closeModal();
+  } else {
+    console.log("wrong data inserted");
+  }
 }
-
 
 function resetForm() {
   // Zurücksetzen der Formularfelder
@@ -208,16 +178,18 @@ function resetForm() {
 
 function displayEventData(event: Event) {
   console.log("display data");
-  (document.getElementById("eventNameUpdate") as HTMLInputElement).value = event.name;
-  (document.getElementById("organizerNameUpdate") as HTMLInputElement).value = event.organization;
-  (document.getElementById("eventDateUpdate") as HTMLInputElement).value = event.date;
-  (document.getElementById("eventLocationUpdate") as HTMLInputElement).value = event.location;
+  (document.getElementById("eventName") as HTMLInputElement).value = event.name;
+  (document.getElementById("organizerName") as HTMLInputElement).value =
+    event.organization;
+  (document.getElementById("eventDate") as HTMLInputElement).value = event.date;
+  (document.getElementById("eventLocation") as HTMLInputElement).value =
+    event.location;
 }
 
 export function removeEvent(id: Number) {
   console.log(id);
   fetch(`/api/events/removeEvent/${id}`, {
-    method: "POST"
+    method: "POST",
   })
     .then((response) => {
       if (response.ok) {
