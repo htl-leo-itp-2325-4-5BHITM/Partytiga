15277dbee422440343190e80c0219aafca8f4a4b
heikov3006 <h.vichtbauer@students.htl-leonding.ac.at>
2024-03-13 19:03:07 +0100

    added tailwind

--- app/frontend/src/components/header-component.ts
+++ app/frontend/src/components/header-component.ts
@@ -18,16 +18,16 @@ export class AppHeaderComponent extends HTMLElement {
   }
   displayHeader() {
     return html` <style>
-        h1 {
+        /*h1 {
           text-transform: uppercase;
           letter-spacing: 1vw;
           font-size: 10vh;
           color: rgb(80, 0, 154);
           text-align: center;
-        }
+        }*/
       </style>
 
-      <h1>Partytiga</h1>`;
+      <h1 >Partytiga</h1>`;
   }
 }
 
